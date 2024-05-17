2b7ec502397bb7f119b00bee54c73c04b1e82a9c
zhangbo1000 <165640986+zhangbo1000@users.noreply.github.com>
2024-05-17 13:46:47 +0800

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -5,129 +5,8 @@
 #include<vector>
 #include<iostream>
 #include<iomanip>
-#include"Types.h"
-#include"Constant.h"
-namespace FFT{
-	//complex numbers
-	using size_t=Types::size_t;
-	//Fast Fourier Transform
-	using cp=Types::complex;
-	using complex=Types::complex;
-	template<const size_t n>
-	void dif(cp a[]){
-		constexpr size_t half=n>>1,quarter=n>>2;
-		cp w(1,0),w3(1,0);
-		const cp wn(cos(Constant::pi2/n),sin(Constant::pi2/n)),wn3(cos(Constant::pi6/n),sin(Constant::pi6/n));
-		for(size_t i=0;i<quarter;i++){
-			if(!(i&2047))w=cp(cos(Constant::pi2*i/n),sin(Constant::pi2*i/n)),w3=w*w*w;
-			const cp tmp1=a[i],tmp2=a[i+quarter],tmp3=a[i+half],tmp4=a[i+half+quarter];
-			cp x=tmp1-tmp3,y=tmp2-tmp4;
-			y=cp(y.imag(),-y.real());
-			a[i]+=tmp3;
-			a[i+quarter]+=tmp4;
-			a[i+half]=(x-y)*w;
-			a[i+half+quarter]=(x+y)*w3;
-			w*=wn;
-			w3*=wn3;
-		}
-		dif<half>(a);
-		dif<quarter>(a+half);
-		dif<quarter>(a+half+quarter);
-	}
-	template<>
-	void dif<1>(cp a[]){}
-	template<>
-	void dif<0>(cp a[]){}
-	template<>
-	void dif<2>(cp a[]){
-		const cp x=a[0],y=a[1];
-		a[0]+=y;
-		a[1]=x-y;
-	}
-	void rundif(cp a[],const size_t& n){
-		switch (n) {
-			case 1<<1:dif<1<<1>(a);break;
-			case 1<<2:dif<1<<2>(a);break;
-			case 1<<3:dif<1<<3>(a);break;
-			case 1<<4:dif<1<<4>(a);break;
-			case 1<<5:dif<1<<5>(a);break;
-			case 1<<6:dif<1<<6>(a);break;
-			case 1<<7:dif<1<<7>(a);break;
-			case 1<<8:dif<1<<8>(a);break;
-			case 1<<9:dif<1<<9>(a);break;
-			case 1<<10:dif<1<<10>(a);break;
-			case 1<<11:dif<1<<11>(a);break;
-			case 1<<12:dif<1<<12>(a);break;
-			case 1<<13:dif<1<<13>(a);break;
-			case 1<<14:dif<1<<14>(a);break;
-			case 1<<15:dif<1<<15>(a);break;
-			case 1<<16:dif<1<<16>(a);break;
-			case 1<<17:dif<1<<17>(a);break;
-			case 1<<18:dif<1<<18>(a);break;
-			case 1<<19:dif<1<<19>(a);break;
-			case 1<<20:dif<1<<20>(a);break;
-			case 1<<21:dif<1<<21>(a);break;
-		}
-	}
-	template<const size_t n>
-	void dit(cp a[]){
-		constexpr size_t half=n>>1,quarter=n>>2;
-		dit<half>(a);
-		dit<quarter>(a+half);
-		dit<quarter>(a+half+quarter);
-		cp w(1,0),w3(1,0);
-		const cp wn(cos(Constant::pi2/n),-sin(Constant::pi2/n)),wn3(cos(Constant::pi6/n),-sin(Constant::pi6/n));
-		for(size_t i=0;i<quarter;i++){
-			if(!(i&2047))w=cp(cos(Constant::pi2*i/n),-sin(Constant::pi2*i/n)),w3=w*w*w;
-			const cp tmp1=w*a[i+half],tmp2=w3*a[i+half+quarter];
-			const cp x=a[i],y=tmp1+tmp2;
-			cp x1=a[i+quarter],y1=tmp1-tmp2;
-			y1=cp(y1.imag(),-y1.real());
-			a[i]+=y;
-			a[i+quarter]+=y1;
-			a[i+half]=x-y;
-			a[i+half+quarter]=x1-y1;
-			w*=wn;
-			w3*=wn3;
-		}
-	}
-	template<>
-	void dit<1>(cp a[]){}
-	template<>
-	void dit<0>(cp a[]){}
-	template<>
-	void dit<2>(cp a[]){
-		const cp x=a[0],y=a[1];
-		a[0]+=y;
-		a[1]=x-y;
-	}
-	void rundit(cp a[],const size_t& n){
-		switch (n) {
-			case 1<<1:dit<1<<1>(a);break;
-			case 1<<2:dit<1<<2>(a);break;
-			case 1<<3:dit<1<<3>(a);break;
-			case 1<<4:dit<1<<4>(a);break;
-			case 1<<5:dit<1<<5>(a);break;
-			case 1<<6:dit<1<<6>(a);break;
-			case 1<<7:dit<1<<7>(a);break;
-			case 1<<8:dit<1<<8>(a);break;
-			case 1<<9:dit<1<<9>(a);break;
-			case 1<<10:dit<1<<10>(a);break;
-			case 1<<11:dit<1<<11>(a);break;
-			case 1<<12:dit<1<<12>(a);break;
-			case 1<<13:dit<1<<13>(a);break;
-			case 1<<14:dit<1<<14>(a);break;
-			case 1<<15:dit<1<<15>(a);break;
-			case 1<<16:dit<1<<16>(a);break;
-			case 1<<17:dit<1<<17>(a);break;
-			case 1<<18:dit<1<<18>(a);break;
-			case 1<<19:dit<1<<19>(a);break;
-			case 1<<20:dit<1<<20>(a);break;
-			case 1<<21:dit<1<<21>(a);break;
-		}
-	}
-}
-namespace Bigint{
+#include"FastFTranslaform.h"
+namespace BigInt{
 	//unsigned long long long int
 	using size_t=Types::size_t;
 	using f64=Types::f64;
@@ -247,7 +126,7 @@ namespace Bigint{
 		return z;
 	}
 	ulllint operator*(const ulllint& x,const ulllint& y){
-		static FFT::cp tmp[ulllint::MAX_SIZE];
+		static FFT::complex tmp[ulllint::MAX_SIZE];
 		static ulllint c;
 		size_t n=x.size()+y.size()-1,s=1;
 		while(s<=n)s<<=1;
@@ -270,7 +149,7 @@ namespace Bigint{
 		return c;
 	}
 	ulllint operator*=(ulllint& x,const ulllint& y){
-		static FFT::cp tmp[ulllint::MAX_SIZE];
+		static FFT::complex tmp[ulllint::MAX_SIZE];
 		size_t n=x.size()+y.size()-1,s=1;
 		while(s<=n)s<<=1;
 		for(size_t i=std::max(x.size(),y.size());i<s;i++)tmp[i]=0; 
