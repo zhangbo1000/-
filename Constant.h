9e07af0fa097dedcd0d82b4d9772acd1f120bff4
zhangbo1000 <165640986+zhangbo1000@users.noreply.github.com>
2024-05-21 13:02:21 +0800

    Update Constant.h

--- Constant.h
+++ Constant.h
@@ -1,7 +1,11 @@
 #ifndef CONSTANT
 #define CONSTANT 1
-#include<cmath>
 namespace Constant{
-	constexpr double pi=acos(-1),pi2=2*acos(-1),pi6=6*acos(-1);
+#ifdef _GLIBCXX_CMATH
+	constexpr double pi=acos(-1),pi2=2*pi,pi6=6*pi;
+#else
+	constexpr double pi=3.141'592'653'589'793'238'46
+	,pi2=2*pi,pi6=6*pi;
+#endif
 }
 #endif
