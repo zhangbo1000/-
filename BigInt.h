fc7dcb4d89a29acf813a2946b0b33d40adf1beed
zhangbo1000 <165640986+zhangbo1000@users.noreply.github.com>
2024-05-22 13:49:37 +0800

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -6,6 +6,31 @@
 #include"FastFTransform.h"
 namespace BigInt{
 	//unsigned long long long int
+	class __BigFloat{
+		using ll=Types::_16int;
+		static constexpr Types::size_t len=4;
+		static constexpr ll base=10000;
+		std::vector<ll>nums;
+		Types::_32int ratio;
+		std::vector<ll>::const_iterator begin()const{return nums.begin();}
+		std::vector<ll>::const_iterator end()const{return nums.end();}
+		Types::size_t size()const{return nums.size();}
+		ll& operator[](const Types::size_t& x){return nums[x];}
+		const ll& operator[](const Types::size_t& x)const{return nums[x];}
+		friend __BigFloat operator+(const __BigFloat&,const __BigFloat&);
+		friend __BigFloat operator-(const __BigFloat&,const __BigFloat&);
+		friend __BigFloat operator*(const __BigFloat&,const __BigFloat&);
+		__BigFloat(const __BigFloat& x){
+			nums=x.nums;
+			ratio=x.ratio;
+		}
+		__BigFloat& operator=(const __BigFloat& x){
+			nums=x.nums;
+			ratio=x.ratio;
+			return *this;
+		}
+		__BigFloat(){};
+	};
 	class ulllint{
 #ifdef _GLIBCXX_IOMANIP
 #ifdef _GLIBCXX_IOSTREAM
@@ -25,7 +50,7 @@ namespace BigInt{
 		ulllint(const std::vector<ll>::const_iterator& first,const std::vector<ll>::const_iterator& last){
 			nums=std::vector<ll>(first,last);
 		}// 用 vector 构造。
-		ulllint(const short& x,const ll& y){
+		ulllint(const Types::_16int& x,const ll& y){
 			nums=std::vector<ll>(x,y);
 		}// 用多个相同数构造，不处理进位。
 		ulllint(const std::vector<ll>&x){nums=x;}
@@ -33,7 +58,7 @@ namespace BigInt{
 		//用整数构造
 		ulllint(const Types::size_t& x=0){
 			nums=std::vector<ll>(1,x);
-			int i=0;
+			Types::size_t i=0;
 			while(nums[i]>base)nums.push_back(nums[i]/base),nums[i++]%=base;
 		}
 		//复制
@@ -51,10 +76,21 @@ namespace BigInt{
 		friend std::ostream& operator<<(std::ostream& cout,const ulllint& x);
 #endif
 #endif
-		friend ulllint operator+(const ulllint& x,const ulllint& y);
-		friend ulllint operator-(const ulllint& x,const ulllint& y);
-		friend ulllint operator*(const ulllint& x,const ulllint& y);
-		friend ulllint operator/(const ulllint& x,const ulllint& y);
+		friend ulllint operator+(const ulllint&,const ulllint&);
+		friend ulllint operator-(const ulllint&,const ulllint&);
+		friend ulllint operator*(const ulllint&,const ulllint&);
+		friend ulllint operator/(const ulllint&,const ulllint&);
+#if __cplusplus >= 202002L	
+		friend auto operator<=>(const ulllint&,const ulllint&);
+#else 
+		friend Types::_8int __comp(const ulllint&,const ulllint&);
+		friend bool operator<(const ulllint&,const ulllint&);
+		friend bool operator>(const ulllint&,const ulllint&);
+		friend bool operator<=(const ulllint&,const ulllint&);
+		friend bool operator>=(const ulllint&,const ulllint&);
+		friend bool operator!=(const ulllint&,const ulllint&);
+#endif
+		friend bool operator==(const ulllint&,const ulllint&);
 		ulllint operator+=(const ulllint& x){
 			if(x.size()>size()){
 				nums.resize(x.size()+1);
@@ -88,7 +124,7 @@ namespace BigInt{
 		std::cin>>temp;
 		const Types::size_t l=strlen(temp);
 		Types::size_t j=-1,w=1;
-		x.nums.resize(l/ulllint::len+2);
+		x.nums=std::vector<ulllint::ll>(l/ulllint::len+2,0);
 		for(Types::size_t i=0;i<l;i++){
 			if(!(i%ulllint::len))j++,w=1;
 			x[j]+=(temp[l-i-1]^'0')*w;
@@ -164,16 +200,46 @@ namespace BigInt{
 		for(Types::size_t i=0;i<s;i++)tmp[i]*=tmp[i];
 		FFT::rundit(tmp,s);
 		x.nums.resize(s);
-		const int kkk=s;
-		const double change=0.5/kkk;
-		unsigned long long int _tmp=0;
+		const Types::_32int kkk=s;
+		const Types::f64 change=0.5/kkk;
+		Types::u64int _tmp=0;
 		for(Types::size_t i=0;i<s;i++){
-			_tmp+=(unsigned long long int)((tmp[i].imag()*change)+0.5);
+			_tmp+=(Types::u64int)((tmp[i].imag()*change)+0.5);
 			x[i]=_tmp%ulllint::base;
 			_tmp/=ulllint::base;
 		}
 		while(x.size()>1&&x[x.size()-1]==0)x.nums.pop_back();
 		return x;
 	}
+#if __cplusplus >= 202002L
+	auto operator<=>(const ulllint& x,const ulllint& y){
+		if(x.size()<y.size())return -1;
+		if(x.size()>y.size())return 1;
+		for(int i=x.size()-1;i>=0;i--){
+			if(x[i]>y[i])return 1;
+			if(x[i]<y[i])return -1;
+		}
+		return 0;
+	}
+	bool operator==(const ulllint& x,const ulllint& y){
+		return (x<=>y)==0;
+	}
+#else
+	Types::_8int __comp(const ulllint& x,const ulllint& y){
+		if(x.size()<y.size())return -1;
+		if(x.size()>y.size())return 1;
+		for(int i=x.size()-1;i>=0;i--){
+			if(x[i]>y[i])return 1;
+			if(x[i]<y[i])return -1;
+		}
+		return 0;
+	}
+	bool operator<(const ulllint& x,const ulllint& y){return __comp(x,y)<0;}
+	bool operator>(const ulllint& x,const ulllint& y){return __comp(x,y)>0;}
+	bool operator<=(const ulllint& x,const ulllint& y){return __comp(x,y)<=0;}
+	bool operator>=(const ulllint& x,const ulllint& y){return __comp(x,y)>=0;}
+	bool operator==(const ulllint& x,const ulllint& y){return __comp(x,y)==0;}
+	bool operator!=(const ulllint& x,const ulllint& y){return __comp(x,y)!=0;}
+#endif
 }
 #endif
